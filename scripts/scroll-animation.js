ec60844221196feeddb90797fe5497922c45bf35
nydia-subur <nydia.subur.2016@sis.smu.edu.sg>
2024-06-17 00:41:27 +0700

    added x axis animation function

--- scripts/scroll-animation.js
+++ scripts/scroll-animation.js
@@ -1,3 +1,4 @@
+//animation code for down up y axis animation
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
@@ -14,3 +15,25 @@ const observer = new IntersectionObserver(
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((hiddenElement) => observer.observe(hiddenElement));
+
+//animation code for left to right x axis animation
+const observer2 = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate-in-x-position");
+      } else {
+        entry.target.classList.remove("animate-in-x-position"); // Delay to match the animation duration
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+const hiddenElementsInXAxis = document.querySelectorAll(
+  ".hiddenElements-x-axis"
+);
+hiddenElementsInXAxis.forEach((hiddenElement2) =>
+  observer2.observe(hiddenElement2)
+);
