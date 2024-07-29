252b21a5e40c4e93ce9077bdfecfdaa65cceaa74
nydia-subur <nydia.subur.2016@sis.smu.edu.sg>
2024-07-30 01:36:37 +0700

    revamp home page- hero and highlighted project section

--- scripts/scroll-animation.js
+++ scripts/scroll-animation.js
@@ -37,3 +37,33 @@ const hiddenElementsInXAxis = document.querySelectorAll(
 hiddenElementsInXAxis.forEach((hiddenElement2) =>
   observer2.observe(hiddenElement2)
 );
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper", {
+    effect: "cube",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true, // Ensure pagination is clickable
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    // Ensure swiping works
+    simulateTouch: true,
+    touchRatio: 1,
+    touchAngle: 45,
+    grabCursor: true,
+    // Optional: Adjust the speed of swiping
+    speed: 800,
+  });
+});
